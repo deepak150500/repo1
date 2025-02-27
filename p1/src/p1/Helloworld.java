bee79ae1934939423b94a85ae01f85994fd4253f
DEEPAK <DEEPAK@LAPTOP-L46A3E47>
2025-02-27 11:20:19 +0530

    hello

---
+++ p1/src/p1/Helloworld.java
@@ -0,0 +1,9 @@
+package p1;
+
+import java.lang.reflect.Method;
+
+public class Helloworld {
+ public static void main(String[] args) {
+	 System.out.println("Hello world");
+ }
+}
---
+++ p1/src/p1/Main3.java
@@ -0,0 +1,8 @@
+package p1;
+
+public class Main3 {
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+
+}
